841c8ebfb04ffb012f49d47a09078ea6e3bd28e3
GISADan-eng <d.gisa2@alustudent.com>
2026-03-18 14:02:20 +0000

    Add 0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
+const filePath = process.argv[2];
+fs.readFile(filePath, 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
-    console.log(data.toString());
+    console.log(data);
   }
 });
