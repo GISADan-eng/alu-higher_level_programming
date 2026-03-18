271987a003c5b2fe29e92b552c3c3c7c85f2370e
GISADan-eng <d.gisa2@alustudent.com>
2026-03-18 13:53:59 +0000

    Add 101-sorted.js

---
+++ javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+const newDict = {};
+for (const userId in dict) {
+  const occ = dict[userId];
+  if (newDict[occ] === undefined) {
+    newDict[occ] = [];
+  }
+  newDict[occ].push(userId);
+}
+console.log(newDict);
